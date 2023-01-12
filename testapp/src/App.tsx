062bfe4b7370329eac116d539489634ed475506a
yeongjin Ha <eclips1024@naver.com>
2023-01-12 18:17:41 +0900

    React & TS Input UseState Study

--- testapp/src/App.tsx
+++ testapp/src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import Myform from './components/Myform';
 import Counter from './components/Counter';
 import Greeting from './components/Greeting'
 
+//Input React&TS 사용하여 상태관리하는 법 
 const App:React.FC=()=>{
+  const onSubmit = (form:{name:string,description:string})=>{
+    console.log(form)
+  }
+  return <Myform onSubmit={onSubmit}/>
+}
+
+// 기존 react UseState 및 컴포넌트 TS로 만들어보기 (간단실습)
+/* const App:React.FC=()=>{
   const onClick = (name:string)=>{
     alert(`${name} says hello`);
   }
   return <Counter/>
   // return <Greeting name="React" onClick={onClick}/>
 }
-
+ */
 export default App;
---
+++ testapp/src/components/Myform.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useState } from 'react';
+
+type MyformsProps={
+    onSubmit:(form:{name:string; description:string})=>void;
+}
+
+function Myform({onSubmit}:MyformsProps) {
+    const [form,setForm] = useState({name:'',description:''});
+    const {name,description} = form;
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
+        //event 인자값 타입을 정하지 못하였을때, any로 모든 타입을 적용시킨다?
+        // 이후 작성한 onChange 함수의 도움말에서 제공되는 타입을 확인한 뒤 타입으로 지정시켜줌(React.ChangeEventHandler<HTMLInputElement>)
+        const {name,value} = e.target;
+        setForm({
+            ...form,
+            [name]:value
+        });
+    };
+    const handlerSubmit = (e: React.FormEvent<HTMLFormElement>)=>{
+        //여기도 동일하게 적용
+        e.preventDefault(); // 기존에 걸린 이벤트 막기
+        onSubmit(form);
+        setForm({
+            name:'',
+            description:'',
+        })//다시 처음 공백으로 초기화시킴
+    }
+    return (
+        <div>
+            <form action="" onSubmit={handlerSubmit}>
+                <input name='name' value={name} onChange={onChange} />
+                <input name='description' value={description} onChange={onChange} />
+            <button type='submit'>등록하기</button>
+            </form>
+        </div>
+    );
+}
+
+export default Myform;
