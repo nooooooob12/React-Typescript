f6b5619bc1d07a578bf6f8a13ae14e28d939dfff
yeongjin Ha <eclips1024@naver.com>
2023-01-16 18:11:16 +0900

    리액트 & 단일 Props

---
+++ react_study/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hello from './components/Hello';
+
+const App = () => {
+    return (
+        <div>
+            <Hello name="hayeongjin" color="blue"></Hello>
+        </div>
+    );
+};
+
+export default App;
