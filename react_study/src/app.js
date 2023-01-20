bbe171c43aade7731f2fd793df930f8c597c8498
yeongjin Ha <eclips1024@naver.com>
2023-01-21 05:02:39 +0900

    배열 추가하기  + useRef + UseState 사용

--- react_study/src/app.js
+++ react_study/src/app.js
@@ -1,22 +1,64 @@
-import React from 'react';
-import Hello from './components/Hello';
-import Input from './components/Input';
-import MultiProps from './components/MultiProps';
+import React, { useRef, useState } from 'react';
 import UserList from './components/UserList';
-import Wrapper from './components/Wrapper';
+import CreateUser from './components/CreateUser';
 
-const App = () => {
+function App() {
+    const [inputs, setInputs] = useState({
+      username: '',
+      email: ''
+    });
+    const { username, email } = inputs;
+    const onChange = e => {
+      const { name, value } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value
+      });
+    };
+    const [users, setUsers] = useState([
+      {
+        id: 1,
+        username: 'velopert',
+        email: 'public.velopert@gmail.com'
+      },
+      {
+        id: 2,
+        username: 'tester',
+        email: 'tester@example.com'
+      },
+      {
+        id: 3,
+        username: 'liz',
+        email: 'liz@example.com'
+      }
+    ]);
+  
+    const nextId = useRef(4);
+    const onCreate = () => {
+      const user = {
+        id: nextId.current,
+        username,
+        email
+      };
+      setUsers(users.concat(user));
+  
+      setInputs({
+        username: '',
+        email: ''
+      });
+      nextId.current += 1;
+    };
     return (
-        <div>
-            <Wrapper>
-            <Hello name="hayeongjin"></Hello>
-            <MultiProps name="Yolo" color="red" isSpacial></MultiProps>
-            <MultiProps/>
-            <Input></Input> 
-            <UserList></UserList>
-            </Wrapper> 
-        </div> //3번째 Tag의 경우, defaultProps를 지정함으로써 프롭스값을 지정해주지 않아도 기존값이 설정되어 들어가 렌더링됨.
+      <>
+        <CreateUser
+          username={username}
+          email={email}
+          onChange={onChange}
+          onCreate={onCreate}
+        />
+        <UserList users={users} />
+      </>
     );
-};
-
-export default App;
+  }
+  
+  export default App;
--- react_study/src/components/UserList.js
+++ react_study/src/components/UserList.js
@@ -1,37 +1,21 @@
 import React from 'react';
 
-function Users({user}) {
-    return(
-        <div>
-            <b>{user.userName}{user.age}</b>
-        </div>
-    )
-}
-
-const UserList = () => {
-    const  user = [
-        {
-            id:"one",
-            userName : "chobo",
-            age:14
-        },
-        {
-            id:"two",
-            userName:"chobo2",
-            age:15
-    }
-    ]
+function User({ user }) {
     return (
-        <div>
-            <ul>
-                <li>
-                    {user.map(user=>(
-                        <Users user={user} key={user.id}></Users>
-                    ))}
-                </li>
-            </ul>
-        </div>
+      <div>
+        <b>{user.username}</b> <span>({user.email})</span>
+      </div>
     );
-};
+  }
+  
+  function UserList({users}) {
+    return (
+      <div>
+        {users.map(user => (
+          <User user={user} key={user.id} />
+        ))}
+      </div>
+    );
+  }
 
 export default UserList;
